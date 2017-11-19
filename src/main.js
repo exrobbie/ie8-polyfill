884f39484c02d3c9b3b835f221d5874eee8e85b3
Robbie <10761269+exrobbie@users.noreply.github.com>
2017-11-19 20:35:33 +0800

    force Google Chrome Frame rendering

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
+var head = document.getElementsByTagName('head')[0];
+
 //fix font-face problem
-var head = document.getElementsByTagName('head')[0],
-    style = document.createElement('style');
+var style = document.createElement('style');
 style.type = 'text/css';
 style.styleSheet.cssText = ':before,:after{content:none !important}';
 head.appendChild(style);
 setTimeout(function(){
     head.removeChild(style);
 }, 0);
+
+//force edge IE rendering and use Google Chrome Frame if possible
+var meta = document.createElement('meta');
+meta.httpEquiv = 'X-UA-Compatiable';
+meta.content = 'IE=edge, chrome=1';
+head.appendChild(meta);
 
 /**
  * respond.js
